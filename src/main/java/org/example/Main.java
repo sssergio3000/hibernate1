1f6955202646a02bf593d61d9e86a611c9eb3084
Sergey Khaykov <ssserg3000@gmail.com>
2023-04-17 20:23:33 +0300

    looks like works right now

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,9 @@
 package org.example;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
-import javax.security.auth.login.Configuration;
+//import javax.security.auth.login.Configuration;
 
 import org.hibernate.cfg.Configuration;
 
@@ -10,7 +11,14 @@ public class Main {
     public static void main(String[] args) {
         Animal cat = new Animal(1L, "Barsik",3,true );
 
-        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory;
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(cat);
+        session.getTransaction().commit();
+        session.close();
+        sessionFactory.close();
+
 
     }
 }
